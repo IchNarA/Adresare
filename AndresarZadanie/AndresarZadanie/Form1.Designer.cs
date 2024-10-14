d06a29807930a97ad7251e3e49bc0f706ffd4165
IchNarA <denishrica7@gmail.com>
2024-10-14 13:52:29 +0200

    Add files via upload

---
+++ AndresarZadanie/AndresarZadanie/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace AndresarZadanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.button_Show = new System.Windows.Forms.Button();
+            this.directory_panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(872, 588);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adresár";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(954, 592);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(120, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // button_Show
+            // 
+            this.button_Show.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button_Show.Location = new System.Drawing.Point(54, 86);
+            this.button_Show.Name = "button_Show";
+            this.button_Show.Size = new System.Drawing.Size(136, 46);
+            this.button_Show.TabIndex = 2;
+            this.button_Show.Text = "Vypísať";
+            this.button_Show.UseVisualStyleBackColor = false;
+            this.button_Show.Click += new System.EventHandler(this.button_Show_Click);
+            // 
+            // directory_panel
+            // 
+            this.directory_panel.AutoScroll = true;
+            this.directory_panel.BackColor = System.Drawing.Color.Gainsboro;
+            this.directory_panel.Location = new System.Drawing.Point(272, 69);
+            this.directory_panel.Name = "directory_panel";
+            this.directory_panel.Size = new System.Drawing.Size(802, 505);
+            this.directory_panel.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(533, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 30);
+            this.label2.TabIndex = 4;
+            // 
+            // backButton
+            // 
+            this.backButton.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.backButton.Image = global::AndresarZadanie.Properties.Resources.back_arrow__1_;
+            this.backButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.backButton.Location = new System.Drawing.Point(272, 28);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(135, 35);
+            this.backButton.TabIndex = 5;
+            this.backButton.Text = "Späť";
+            this.backButton.UseVisualStyleBackColor = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 648);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.directory_panel);
+            this.Controls.Add(this.button_Show);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button button_Show;
+        private System.Windows.Forms.FlowLayoutPanel directory_panel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button backButton;
+    }
+}
+
---
+++ AndresarZadanie/AndresarZadanie/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AndresarZadanie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int filescount = 0;
+        int directoriescount = 0;
+        string currentLocation = "";
+        long directorySize = 0;
+        
+        Random Random = new Random();
+        
+        public void Struktury(string adresar)
+        {
+            directory_panel.Controls.Clear();
+            string[] subory = Directory.GetFiles(adresar);
+            string[] adresare = Directory.GetDirectories(adresar);
+
+
+            Label nadpisLabel = new Label();
+            nadpisLabel.Text = "Súbory v tomto adresári:";
+            nadpisLabel.AutoSize = true;
+            Font f = new Font("Arial",15,FontStyle.Bold);
+            nadpisLabel.Font = f;
+            directory_panel.Controls.Add(nadpisLabel);
+            directory_panel.SetFlowBreak(nadpisLabel, true);
+            
+
+            if (subory.Length > 0)
+            {
+                
+                foreach (string s in subory)
+                {
+                    FileInfo fileInfo = new FileInfo(s);
+                    Label newLabel = new Label();
+                    newLabel.Text = fileInfo.Name + " (" + Prevod((long)fileInfo.Length) + ")";
+                    newLabel.Name = s;
+                    newLabel.AutoSize = true;
+                    Font a = new Font("Arial", 10);
+                    newLabel.Font = a;
+                    directory_panel.Controls.Add(newLabel);
+                    filescount++;
+                    SaveToFile(s);
+                    directory_panel.SetFlowBreak(newLabel, true);
+                }
+                
+            }
+            else
+            {
+                Label newLabel = new Label();
+                newLabel.Text = "V tomto adresári neexistuje súbor!";
+                Font a = new Font("Arial", 10);
+                newLabel.AutoSize = true;
+                newLabel.Font = a;
+                directory_panel.Controls.Add(newLabel);
+                directory_panel.SetFlowBreak(newLabel, true);
+            }
+
+            
+            Label filescounttext = new Label();
+            filescounttext.Text = "Počet súborov v adresári: " + filescount;
+            filescounttext.AutoSize = true;
+            filescounttext.Font = f;
+            directory_panel.Controls.Add(filescounttext);
+            directory_panel.SetFlowBreak(filescounttext, true);
+
+            Panel linePanel = new Panel();
+            linePanel.BackColor = Color.Orange;
+            linePanel.Size = new Size(800, 10);
+            directory_panel.Controls.Add(linePanel);
+            directory_panel.SetFlowBreak(linePanel, true);
+
+            Label nadpisLabel2 = new Label();
+            nadpisLabel2.Text = "Podadresáre:";
+            nadpisLabel2.AutoSize = true;
+            nadpisLabel2.Font = f;
+            directory_panel.Controls.Add(nadpisLabel2);
+            directory_panel.SetFlowBreak(nadpisLabel2, true);
+
+            
+
+            if (adresare.Length > 0)
+            {
+                
+                foreach (string s in adresare)
+                {
+                    var startOfName = s.LastIndexOf("\\");
+                    var fileName = s.Substring(startOfName + 1, s.Length - (startOfName + 1));
+                    Button newButton = new Button();
+                    newButton.Image = System.Drawing.Image.FromFile("folder.png");
+                    newButton.BackColor = Color.LightBlue;
+                    newButton.ImageAlign = ContentAlignment.MiddleLeft;
+                    
+                    newButton.FlatStyle = FlatStyle.Flat;
+                    newButton.Text = fileName;
+                    newButton.Name = s;
+                    newButton.Size = new Size(200, 50);
+                    newButton.TextAlign = ContentAlignment.MiddleRight;
+                    Font b = new Font("Elephant", 16, FontStyle.Bold);
+                    newButton.Font = f;
+                    
+                    directoriescount++;
+                    SaveToFile(s);
+                    
+                    directory_panel.Controls.Add(newButton);
+                    newButton.Click += button_click_open;
+                    
+                }
+                
+            }
+            else
+            {
+                Label newLabel = new Label();
+                newLabel.Text = "V tomto adresári niesu žiadne podadresáre";
+                Font a = new Font("Arial", 10);
+                newLabel.AutoSize = true;
+                newLabel.Font = a;
+                directory_panel.Controls.Add(newLabel);
+                directory_panel.SetFlowBreak(newLabel, true);
+            }
+
+            Label emptylabel = new Label();
+            emptylabel.Width = 0;
+            emptylabel.Height = 0;
+            emptylabel.Margin = new Padding(0, 0, 0, 0);
+
+            directory_panel.Controls.Add(emptylabel);
+            directory_panel.SetFlowBreak(emptylabel, true);
+
+            Label directoriescounttext = new Label();
+            directoriescounttext.Text = "Počet podadresátov v adresári: " + directoriescount + " a adresár zaberá: " + Prevod(directorySize);
+            directoriescounttext.AutoSize = true;
+            directoriescounttext.Font = f;
+            directory_panel.Controls.Add(directoriescounttext);
+            directory_panel.SetFlowBreak(directoriescounttext, true);
+            label2.Text = currentLocation;
+            filescount = 0;
+            directoriescount = 0;
+        }
+
+        public string Prevod(long cislo)
+        {
+            if (cislo >= 1000 && cislo < 1000000)
+                return (cislo / 1000.0).ToString("0.00") + "kB";
+            if (cislo >= 1000000 && cislo < 1000000000)
+                return (cislo / 1000000.0).ToString("0.00") + "MB";
+            if (cislo > 1000000000)
+                return (cislo / 1000000000.0).ToString("0.00") + "GB";
+            else
+                return cislo.ToString() + "B";
+        }
+
+        public void SaveToFile(string text)
+        {
+            bool filefound = false;
+            if (!File.Exists("vystup.txt"))
+                File.AppendAllText("vystup.txt", text + "\n");
+            string[] riadky = File.ReadAllLines("vystup.txt");
+            foreach (string riadok in riadky)
+            {
+                if (riadok == text)
+                {
+                    filefound = true;
+                    break;
+                }
+            }
+
+            if (filefound == false)
+                File.AppendAllText("vystup.txt", text + "\n");
+        }
+
+
+        public void button_click_open(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+            string filepath = button.Name;
+            currentLocation = filepath;
+            DirectoryInfo directory = new DirectoryInfo(currentLocation);
+            directorySize = directory.EnumerateFiles("*", SearchOption.AllDirectories).Sum(file => file.Length);
+            Struktury(filepath);
+        }
+
+        
+
+        private void button_Show_Click(object sender, EventArgs e)
+        {
+            
+            string menoadr = inputBox.Text;  
+            try
+            {
+                if (String.IsNullOrEmpty(menoadr))
+                    throw new Exception("Musíš zadať adresár!");
+                DirectoryInfo directory = new DirectoryInfo(menoadr);
+                directorySize = directory.EnumerateFiles("*", SearchOption.AllDirectories).Sum(file => file.Length);
+                currentLocation = menoadr;
+                Struktury(menoadr);
+            }
+            catch(DirectoryNotFoundException)
+            {
+                MessageBox.Show("Takýto adresár neexistuje!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+           try
+            {
+                string previousLocation = currentLocation.Substring(0, currentLocation.LastIndexOf("\\"));
+                currentLocation = previousLocation;
+                DirectoryInfo directory = new DirectoryInfo(currentLocation);
+                directorySize = directory.EnumerateFiles("*", SearchOption.AllDirectories).Sum(file => file.Length);
+                Struktury(currentLocation);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
